553da2c17ddfb1c177a3b26dd3f9eee19ffe0310
Shaun Hutch <shaunoff@Shauns-MBP.home>
2018-01-18 12:22:34 -0500

    Initial Commit

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,7 +1,9 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
-
+    socket.on('test', () => {
+      console.log(`Hello`)
+    })
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
